08b911dc79e046285bf37687bdd77e7993ee0e21
Alla <allanudelman@gmail.com>
2023-05-10 22:55:17 +0300

    some changes

---
+++ Week_2/W2_Day_4/DailyChallenge/Daily Challenge.py
@@ -0,0 +1,33 @@
+matrix_text = '''
+    7i3
+    Tsi
+    h%x
+    i #
+    sM 
+    $a 
+    #t%
+    ^r!
+'''
+
+matrix_text = matrix_text.replace("\n", "")
+matrix_list = []
+for simbol in matrix_text: 
+    if simbol !=' ':
+        matrix_list.append(simbol)
+print(f'Step 1: {matrix_list}')
+matrix = []
+m = []
+string = ""
+while matrix_list != []:
+    matrix.append(matrix_list[:3])
+    matrix_list = matrix_list[3:]
+print(f'Step 2: {matrix}') 
+
+for j in range(3):
+    for i in range(len(matrix)):
+        if matrix[i][j].isalpha():
+            string += matrix[i][j]
+        # else:
+        #     string += " "
+print(f'Step 3: {string}')
+

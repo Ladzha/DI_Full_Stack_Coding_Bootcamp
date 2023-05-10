3385e83ffcda70d682a3cbdcb4fa2321b3c1cc5b
Alla <allanudelman@gmail.com>
2023-05-10 07:35:37 +0300

    finished daily challenge 1

--- Week_2/W2_Day_3/DailyChallenge/Daily Challenge.py
+++ Week_2/W2_Day_3/DailyChallenge/Daily Challenge.py
@@ -15,30 +15,30 @@ Examples
 "grapes" ➞ { "g": [0], "r": [1], "a": [2], "p": [3]}
 
 '''
+print("Challenge 1")
+word = input("Write a word: ")
+list_index = [num for num in range(len(word))]
+my_dictionary = dict(zip(word, list_index))
+print(my_dictionary )
 
-# word = input("Write a word: ")
-# list_index = [num for num in range(len(word))]
-# my_dictionary = {key:value for key in word, value in list_index}
-# print(my_dictionary )
 
-
-store = {
-  "Water": "1",
-  "Bread": "3",
-  "TV": "1,000",
-  "Fertilizer": "20",
-  "Apple" : "4",
-  "Fan": "14",
-  "Pan": "100",
-  "Spoon": "2"
-}
-wallet = 30
-# price_item = 0
-can_buy = []
-for i in store.values():
-    if int(i) < wallet:
-        can_buy.append(store)
-        wallet -=i
-print(can_buy)
+# store = {
+#   "Water": "1",
+#   "Bread": "3",
+#   "TV": "1,000",
+#   "Fertilizer": "20",
+#   "Apple" : "4",
+#   "Fan": "14",
+#   "Pan": "100",
+#   "Spoon": "2"
+# }
+# wallet = 30
+# # price_item = 0
+# can_buy = []
+# for i in store.values():
+#     if int(i) < wallet:
+#         can_buy.append(store)
+#         wallet -=i
+# print(can_buy)
 
 

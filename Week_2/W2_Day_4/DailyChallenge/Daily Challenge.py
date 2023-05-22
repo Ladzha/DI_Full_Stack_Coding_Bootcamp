bcc993a5189708dfc5f4d6cf360bda3f4e565349
Alla <allanudelman@gmail.com>
2023-05-22 23:04:18 +0300

    redo Week 2 Day 4 Daily Challenge

--- Week_2/W2_Day_4/DailyChallenge/Daily Challenge.py
+++ Week_2/W2_Day_4/DailyChallenge/Daily Challenge.py
@@ -8,58 +8,24 @@ matrix_text = '''
     #t%
     ^r!
 '''
+
 print(f'Step 0: {matrix_text}')
-# matrix_list = list(matrix_text)
+matrix_text=matrix_text.replace('\n','')
+matrix_text=matrix_text.replace('  ','')
+print(f'Step 1: {matrix_text}')
 matrix_list = []
-
-print(f'Step 1: {matrix_list}')
-
 for i in matrix_text:
-    matrix_list.append(i)
-print(f'Step 1: {matrix_list}')
-   
+    matrix_list.append(i)  
+print(f'Step 2: {matrix_list}')
+matrix = []
+while matrix_list != []:
+    matrix.append(matrix_list[:3])
+    matrix_list = matrix_list[3:]
+print(f'Step 3: {matrix}')
 
-# matrix_text = matrix_text.replace("\n", "")
-# matrix_list = []
-# for simbol in matrix_text: 
-#     if simbol !=' ':
-#         matrix_list.append(simbol)
-# print(f'Step 1: {matrix_list}')
-# matrix = []
-# string = ""
-# while matrix_list != []:
-#     matrix.append(matrix_list[:3])
-#     matrix_list = matrix_list[3:]
-# print(f'Step 2: {matrix}') 
-
-# for j in range(3):
-#     for i in range(len(matrix)):
-#         if matrix[i][j].isalpha():
-#             string += matrix[i][j]
-#         # else:
-#         #     string += " "
-# print(f'Step 3: {string}')
-
-
-# print(f'Step 0: {matrix_text}')
-# matrix_text = matrix_text.replace("\n", "")
-# matrix_list = []
-# for simbol in matrix_text: 
-#     if simbol !=' ':
-#         matrix_list.append(simbol)
-# print(f'Step 1: {matrix_list}')
-# matrix = []
-# string = ""
-# while matrix_list != []:
-#     matrix.append(matrix_list[:3])
-#     matrix_list = matrix_list[3:]
-# print(f'Step 2: {matrix}') 
-
-# for j in range(3):
-#     for i in range(len(matrix)):
-#         if matrix[i][j].isalpha():
-#             string += matrix[i][j]
-#         # else:
-#         #     string += " "
-# print(f'Step 3: {string}')
-
+string = ""
+for col in range(3):  
+    for row in range(len(matrix)):
+        if matrix[row][col].isalpha():
+            string += matrix[row][col]    
+print(f'Step 4: {string}')

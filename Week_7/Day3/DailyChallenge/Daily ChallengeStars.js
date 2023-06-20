258ad4e9e37033aea3b2aa56599607a397eb29b5
Alla <allanudelman@gmail.com>
2023-06-20 08:34:18 +0300

    Week_7 Day3 DailyChallenge2

---
+++ Week_7/Day3/DailyChallenge/Daily ChallengeStars.js
@@ -0,0 +1,15 @@
+let star = ' * ';
+let pattern = '';
+for(let i=1; i<7; i++){
+    pattern +=star
+    console.log(pattern)
+};
+
+let star2 = ' * ';
+let pattern2 = '';
+for(let i=1; i<7; i++){
+    pattern2 +=star2
+    for(let j=1; j<2; j++){
+        console.log(pattern2)
+    }
+};

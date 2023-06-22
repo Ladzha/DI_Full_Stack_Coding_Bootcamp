1cd17af6c617fe50acb81f09abc9916b02f4402b
Alla <allanudelman@gmail.com>
2023-06-22 08:42:54 +0300

    Week_7 Day4 DailyChallenge add bonus

--- Week_7/Day4/DailyChallenge/script.js
+++ Week_7/Day4/DailyChallenge/script.js
@@ -3,9 +3,9 @@
 let section = document.querySelector('section');
 
 // 1 / Create an array which value is the planets of the solar system.
-let planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Neptune'];
+let planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
 
-const colors = ['red', 'yellow', 'blue', 'green', 'white', 'pink', 'lightblue']
+const colors = ['lightgreen', 'yellow', 'blue', 'green', 'white', 'pink', 'lightblue', 'orange']
 
 // 2 / For each planet in the array, create a <div> using createElement. This div should have a class named "planet".
 
@@ -25,50 +25,59 @@ for(let i =0; i< planets.length; i++){
     divForPlanet.classList.add('planet', colors[i]);
     divForPlanet.style.backgroundColor=colors[i];
     section.appendChild(divForPlanet); 
-
 };
 
 
-//BONUD NOT FINISHED
-
-
-// let moons = {
-//     'Mercury': null,
-//     'Venus': null,
-//     'Earth': 'Moon', 
-//     'Mars': ['Phobos', 'Deimos'],
-//     'Jupiter': ['Io', 'Europa', 'Ganymede', 'Callisto'],
-//     'Saturn': ['Titan', 'Enceladus', 'Mimas', 'Rhea'],
-//     'Uranus': ['Miranda', 'Ariel', 'Umbriel', 'Titania', 'Oberon'],
-//     'Neptune': ['Triton', 'Nereid', 'Proteus']
-//     }
-
+//BONUS
 
 
     let moons = [
-        {'Mercury': []},
-        {'Venus': []},
-        {'Earth': ['Moon']}, 
-        {'Mars': ['Phobos', 'Deimos']},
-        {'Jupiter': ['Io', 'Europa', 'Ganymede', 'Callisto']},
-        {'Saturn': ['Titan', 'Enceladus', 'Mimas', 'Rhea']},
-        {'Uranus': ['Miranda', 'Ariel', 'Umbriel', 'Titania', 'Oberon']},
-        {'Neptune': ['Triton', 'Nereid', 'Proteus']}
+        {planet: 'Mercury', moon: []},
+        {planet: 'Venus', moon: []},
+        {planet: 'Earth', moon: ['Moon']}, 
+        {planet: 'Mars', moon: ['Phobos', 'Deimos']},
+        {planet: 'Jupiter', moon: ['Io', 'Europa', 'Ganymede', 'Callisto']},
+        {planet: 'Saturn', moon: ['Titan', 'Enceladus', 'Mimas', 'Rhea']},
+        {planet: 'Uranus', moon: ['Miranda', 'Ariel', 'Umbriel', 'Titania', 'Oberon']},
+        {planet: 'Neptune', moon: ['Triton', 'Nereid', 'Proteus']}
     ]
 
 const colorsMoons = ['Indigo', 'Coral', 'Lavender', 'Turquoise', 'Maroon', 'Teal', 'Plum', 'Amber', 'Slate', 'Periwinkle', 'Mauve', 'Olive', 'Crimson', 'Sapphire', 'Apricot', 'Jade', 'Lilac', 'Chestnut', 'Emerald', 'Rosewood', 'Azure', 'Orchid', 'Brick', 'Coral', 'Cobalt', 'Papaya', 'Mulberry', 'Topaz', 'Sienna', 'Chartreuse']
 
+console.log(moons.planet);
 
     for(let i =0; i< moons.length; i++){
-        console.log(moons[i])
-        for(let j =0; j< moons[i].length; j++){
-            console.log(j)
+        let planet = moons[i].planet;
+        let moonList = moons[i].moon;
 
+        const planetName = document.createTextNode(planets[i])
+        const paragraph = document.createElement('p')
+        paragraph.appendChild(planetName)
+    
+        const divForPlanet = document.createElement('div');
+        divForPlanet.appendChild(paragraph)
+        divForPlanet.style.position += 
+    
+        divForPlanet.classList.add('planet', colors[i]);
+        divForPlanet.style.backgroundColor=colors[i];
+        section.appendChild(divForPlanet); 
+
+
+        for(let j =0; j< moonList.length; j++){
+            console.log(moonList[j])
+
+
+            const moonName = document.createTextNode(moonList[j])
+            const paragraphMoon = document.createElement('p')
+            paragraphMoon.appendChild(moonName)
+        
+            const divForMoon = document.createElement('div');
+            divForMoon.appendChild(paragraphMoon)
+        
+            divForMoon.classList.add('moon', colorsMoons[j]);
+            divForMoon.style.backgroundColor=colorsMoons[j];
+            divForMoon.style.position = 'relative';
+            divForMoon.style.left = '150 px';
+            section.appendChild(divForMoon); 
         }
-
-        // let divMoon = document.createElement('div');
-        // divMoon.classList.add('moon');
-        // divMoon.style.backgroundColor=colorsMoons[i];
-        // section.appendChild(divMoon); 
-
 };

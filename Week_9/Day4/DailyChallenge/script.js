53ed0a1167e5441b40509aa3642b1bb3be5542e6
Alla <allanudelman@gmail.com>
2023-07-05 22:55:40 +0300

    Week_9 Day4 DailyChallenge

--- Week_9/Day4/DailyChallenge/script.js
+++ Week_9/Day4/DailyChallenge/script.js
@@ -1,25 +1,41 @@
 const form = document.getElementById("formConvert");
 
-// const fromCurrency = form.elements.namedItem("fromCurrency");
-// console.log(fromCurrency);
-
-const toCurrency = form.elements.namedItem("toCurrency");
-// console.log(toCurrency);
-
-const amount = form.elements.namedItem("amount");
-console.log(amount);
-
 const convertButton = form.elements.namedItem("convertButton");
-console.log(convertButton);
+// const switchButton = form.elements.namedItem("switchButton");
 
-const result = document.getElementById("result")
-console.log(result);
+convertButton.addEventListener("click", getUserInput)
+// switchButton.addEventListener("click", switchCurrency)
 
+// function switchCurrency(event){
+//     event.preventDefault()
 
-async function getCurrency(){
+//     const fromCurrency = form.elements.namedItem("fromCurrency").value; 
+//     fromCurrency.innerHTML = "lol"
+//     const toCurrency = form.elements.namedItem("toCurrency").value;
     
-    const currencyData = fetch("https://v6.exchangerate-api.com/v6/13e850427b43de4ce15525b3/codes")
-    currencyData
+// }
+
+function getUserInput(event){
+    event.preventDefault()
+    const fromCurrency = form.elements.namedItem("fromCurrency").value;
+    const fromCurrencyArr = fromCurrency.split(" ")
+    const base = fromCurrencyArr[0];
+
+    const toCurrency = form.elements.namedItem("toCurrency").value;
+    const toCurrencyArr = toCurrency.split(" ")
+    const target = toCurrencyArr[0];
+
+    let amountInput = form.elements.namedItem("amount").value;
+    if(amountInput == ''){
+        amountInput ='1'
+    }
+    getCurrency(base, target, amountInput)
+}
+
+//getting list of currency code
+function getCodeData(){   
+    const currencyCodeList = fetch("https://v6.exchangerate-api.com/v6/13e850427b43de4ce15525b3/codes")
+    currencyCodeList
     .then(response =>{
         if(response.ok){
             return response.json()
@@ -29,10 +45,10 @@ async function getCurrency(){
         }
     })
     .then(data =>{
-        let currency = data["supported_codes"]
-        console.log(currency);
-        for(let element of currency){
-            createCurrencyList(`${element[0]} ${element[1]}`)
+        let currencyCodeArray = data["supported_codes"]
+        for(let element of currencyCodeArray){
+            createCurrencyCodeList(`${element[0]} ${element[1]}`)
+            // console.log(element[0]);
         }
     })
     .catch(error=>{
@@ -40,68 +56,39 @@ async function getCurrency(){
     })
 }
 
-getCurrency()
+getCodeData();
 
-let base = 'EUR';
-let target = 'AOA';
+//getting converted result
+function getCurrency(base, target, amount){
+  
+    const pconversionData = fetch(`https://v6.exchangerate-api.com/v6/13e850427b43de4ce15525b3/pair/${base}/${target}/${amount}`)
+    pconversionData
+    .then(response =>{
+        if(response.ok){
+            return response.json()
+        }
+        else{
+            throw new Error("Something went wrong")
+        }
+    })
+    .then(data =>{
+        let conversionResult = data["conversion_result"]
+        displayResult(conversionResult)
 
-// "base_code": "EUR",
-// "target_code": "GBP",
-// "conversion_rate": 0.8412,
-// "conversion_result": 5.8884
+    })
+    .catch(error=>{
+        console.log(error);
+    })
+};
 
+//displaying result in div
+function displayResult(result){
+    const resultDiv = document.getElementById("result");
+    resultDiv.textContent = result; 
+};
 
-// async function getCurrency(base, target){
-    
-//     const pconversionData = fetch("https://v6.exchangerate-api.com/v6/13e850427b43de4ce15525b3/pair/EUR/GBP/AMOUNT")
-//     // const pconversionData = fetch(`https://v6.exchangerate-api.com/v6/13e850427b43de4ce15525b3/pair/${base}/${target}`)
-//     pconversionData
-//     .then(response =>{
-//         if(response.ok){
-//             return response.json()
-//         }
-//         else{
-//             throw new Error("Something went wrong")
-//         }
-//     })
-//     .then(data =>{
-//         let conversionRate = data["conversion_rate"]
-//         console.log(conversionRate);
-
-//     })
-//     .catch(error=>{
-//         console.log(error);
-//     })
-// }
-
-// getCurrency(base, target)
-
-
-
-
-const pconversionData = fetch("https://v6.exchangerate-api.com/v6/13e850427b43de4ce15525b3/pair/EUR/GBP/AMOUNT")
-// const pconversionData = fetch(`https://v6.exchangerate-api.com/v6/13e850427b43de4ce15525b3/pair/${base}/${target}`)
-// pconversionData
-// .then(response =>{
-//     if(response.ok){
-//         return response.json()
-//     }
-//     else{
-//         throw new Error("Something went wrong")
-//     }
-// })
-// .then(data =>{
-//     let conversionRate = data["conversion_rate"]
-//     console.log(conversionRate);
-
-// })
-// .catch(error=>{
-//     console.log(error);
-// })
-
-
-
-function createCurrencyList(currencyCode){
+//creating list of currency code
+function createCurrencyCodeList(currencyCode){
     const selectFromCurrency = form.elements.namedItem("fromCurrency");
     const selectToCurrency = form.elements.namedItem("toCurrency")
 
@@ -109,19 +96,22 @@ function createCurrencyList(currencyCode){
 
     const optionFromCurrency = document.createElement('option');
     optionFromCurrency.setAttribute('value', currencyName);
+    optionFromCurrency.classList.add('option')
+
+    if(optionFromCurrency.value =='EUR Euro'){
+        optionFromCurrency.setAttribute('selected', 'selected');
+    }
     optionFromCurrency.textContent=currencyName;
     selectFromCurrency.appendChild(optionFromCurrency);
 
     const optionToCurrency = document.createElement('option');
     optionToCurrency.setAttribute('value', currencyName);
+    optionToCurrency.classList.add('option')
+
+    if(optionToCurrency.value =='USD United States Dollar'){
+        optionToCurrency.setAttribute('selected', 'selected');
+    }
     optionToCurrency.textContent=currencyName;
 
     selectToCurrency.appendChild(optionToCurrency);
-}
-
-
-
-// console.log(selectFromCurrency);
-
-// const selectToCurrency = form.elements.namedItem("toCurrency");
-// console.log(selectToCurrency);
+};
